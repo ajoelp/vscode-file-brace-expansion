f756587cb3880eb0dd03ca2f5a3c99f214bd5ebd
Joel Podrebarac <joelpodrebar@gmail.com>
2019-03-27 10:21:47 -0400

    Parse braces before folder creation

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,9 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as fs from 'fs-extra';
 import * as path from 'path';
 import * as braces from 'braces';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "multifile" is now active!');
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
     'extension.createFile',
     async file => {
@@ -28,37 +17,35 @@ export function activate(context: vscode.ExtensionContext) {
       }
 
       try {
-        const answer = await vscode.window.showInputBox({
+        const bracedPath = await vscode.window.showInputBox({
           prompt: 'Filename:',
           placeHolder: 'test.js'
         });
-        if (!answer) {
+        if (!bracedPath) {
           throw new Error('Cannot be empty');
         }
 
-        const newPath = path.resolve(rootPath, answer);
+        const generatedFileNames = braces.expand(bracedPath);
 
-        await fs.mkdirp(path.dirname(newPath));
+        for (const fileIndex in generatedFileNames) {
+          const file = generatedFileNames[fileIndex];
+          const fullPath = path.resolve(rootPath, file);
 
-        const files = braces.expand(newPath);
+          await fs.mkdirp(path.dirname(fullPath));
 
-        for (const fileIndex in files) {
-          const file = files[fileIndex];
-          if (fs.existsSync(file)) {
+          if (fs.existsSync(fullPath)) {
             throw new Error('File already exists');
           }
-          await fs.writeFile(file, '');
+
+          await fs.writeFile(fullPath, '');
         }
       } catch (e) {
         vscode.window.showWarningMessage(e.message);
       }
-
-      // Display a message box to the user
     }
   );
 
   context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
